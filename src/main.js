a98b767132593fd375b5ff6404f37ffb1fc24d60
JP.Zhou <3214423826@qq.com>
2024-03-18 15:56:49 +0800

    完成element组件的引入并使用

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 引入element组件样式
+import 'element-ui/lib/theme-chalk/index.css'
+// 按需引入element组件
+import { Button, Select } from 'element-ui'
+Vue.use(Button)
+Vue.use(Select)
+
 Vue.config.productionTip = false
 
 new Vue({
