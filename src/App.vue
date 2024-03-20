7e65f8c0fb3c1e743dca9110521fa2343cffe322
JP.Zhou <3214423826@qq.com>
2024-03-20 11:01:26 +0800

    完成左侧导航栏组件的搭建

--- src/App.vue
+++ src/App.vue
@@ -6,32 +6,7 @@
         <!-- 左边logo -->
         <logo />
         <!-- 左边导航栏 -->
-        <el-menu default-active="1" class="el-menu-vertical-demo">
-          <el-menu-item index="1">
-            <i class="el-icon-s-home"></i>
-            <span slot="title">首页</span>
-          </el-menu-item>
-          <el-menu-item index="2">
-            <i class="el-icon-s-grid"></i>
-            <span slot="title">部门</span>
-          </el-menu-item>
-          <el-menu-item index="3">
-            <i class="el-icon-user-solid"></i>
-            <span slot="title">成员</span>
-          </el-menu-item>
-          <el-menu-item index="4">
-            <i class="el-icon-money-rmb-symbol"></i>
-            <span slot="title">薪资</span>
-          </el-menu-item>
-          <el-menu-item index="5">
-            <i class="el-icon-share"></i>
-            <span slot="title">关系</span>
-          </el-menu-item>
-          <el-menu-item index="6">
-            <i class="el-icon-chat-square"></i>
-            <span slot="title">消息</span>
-          </el-menu-item>
-        </el-menu>
+        <common-aside />
       </el-aside>
       <!-- 右边主体内容 -->
       <el-container>
@@ -46,11 +21,13 @@
 <script>
 // 引入logo组件
 import logo from "@/components/logo"
-
+// 引入CommonAside组件
+import CommonAside from "@/components/CommonAside"
 export default {
   name: 'App',
   components: {
-    logo
+    logo,
+    CommonAside
   }
 }
 </script>
---
+++ src/components/CommonAside/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <el-menu default-active="1" class="el-menu-vertical-demo">
+    <el-menu-item v-for="item in asideItem" :key="item.index" :index="item.index">
+      <i :class="`el-icon-${item.icon}`"></i>
+      <span slot="title">{{ item.content }}</span>
+    </el-menu-item>
+</el-menu>
+</template>
+
+<script>
+export default {
+  name: "CommonAside",
+  data() {
+    return {
+      asideItem: [
+        {
+          index: 1,
+          icon: "s-home",
+          content: "首页"
+        },
+        {
+          index: 2,
+          icon: "s-grid",
+          content: "部门"
+        },
+        {
+          index: 3,
+          icon: "user-solid",
+          content: "成员"
+        },
+        {
+          index: 4,
+          icon: "money-rmb-symbol",
+          content: "薪资"
+        },
+        {
+          index: 5,
+          icon: "share",
+          content: "关系"
+        },
+        {
+          index: 6,
+          icon: "chat-square",
+          content: "消息"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
